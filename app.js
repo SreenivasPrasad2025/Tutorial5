b86dbf21c655356e8b7100cfcf5cd1154b8d3824
VenkataSreenivasPrasad <137905440+SreenivasPrasad1998@users.noreply.github.com>
2024-03-10 22:42:46 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express"); // Import express
+const application = express(); // Create an express application
+application.use(express.json()); // Middleware to parse JSON
+
+const port = 5500; // Port number
+
+let DbUsers = [
+  { email: "abc@abc.ca", firstName: "ABC", id: "7hYD438" },
+  { email: "xyz@xyz.ca", firstName: "XYZ", id: "Pjh43h4" },
+];
+
+const findUserById = (id) => DbUsers.find((user) => user.id === id);
+
+let randomGenerateId = () => {
+  let id_length = 7;
+  let final_result = "";
+  const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const totalLength = char.length;
+  for (let i = 0; i < id_length; i++) {
+    final_result += char.charAt(Math.floor(Math.random() * totalLength));
+  }
+  return final_result;
+};
+
+application.get("/users", (req, res) => {
+  res.json({
+    message: "Users retrieved",
+    success: true,
+    users: DbUsers,
+  });
+});
+
+application.post("/add", (req, res) => {
+  const { email, firstName } = req.body;
+  const newUser = {
+    email: email,
+    firstName: firstName,
+    id: randomGenerateId(),
+  };
+  DbUsers.push(newUser);
+  res.status(201).json({
+    message: "User added",
+    success: true,
+  });
+});
+
+application.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { email, firstName } = req.body;
+  const user = findUserById(id);
+  if (!user) {
+    return res
+      .status(404)
+      .json({ message: "User not found in the Database", success: false });
+  }
+  user.email = email || user.email;
+  user.firstName = firstName || user.firstName;
+  res.json({
+    message: "User updated",
+    success: true,
+  });
+});
+
+application.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+  const user = findUserById(id);
+  if (!user) {
+    return res.status(404).json({ message: "User not found", success: false });
+  }
+  res.json({
+    success: true,
+    user: user,
+  });
+});
+
+application.listen(port);
